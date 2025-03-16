2ec987c6f78f3bdb6fca9544619db745fe8c4a6a
qingyuan lu <qingyuanhuang@foxmail.com>
2025-03-16 15:12:26 +0800

    添加项目文件。

---
+++ Unity_Intern_Day01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Unity_Intern_Day01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
